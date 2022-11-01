369d71ded629f77273ea0a06aa91d021b3e9f122
Sainty93 <jenandfloyd93@icloud.com>
2022-10-31 23:16:00 -0700

    added javascript

--- Develop/script.js
+++ Develop/script.js
@@ -23,8 +23,47 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword () {
-  var passwordlength = prompt("12");
-  var numbers = confirm("1234567890");
-  var lowerCases = confirm("qwertyuioplkjhgfdsazxcvbnm");
-  var uppercases = confirm("QWERRTYUIOPLKJHGFDSAZXCVBNM")
+  var passwordlength = prompt("Enter password length");
+  var includeLowercase = prompt('Include lowercase? (yes/no)');
+  
+  if (passwordlength < 8 || passwordlength > 128) {
+    alert('invalid length');
+    console.log("invalid length");
+  }
+  if (password.hasUpperCase){
+    console.log("Your passowrd includes uppercase letters");
+    return;
+  } else {
+    console.log("Your password does not include uppercase letters");
+    }
+  if (password.hasLowercase){
+    console.log("your password icludes lower case letter");
+    return;
+  } else {
+    console.log("Your password does not include lower case letter");
+    }
+
+  if (!includeLowercase && !includleNumeric && !includeUppercase) {
+    console.log(i)
+  }
+
+  // run a for loop according to the length of the password
+  var resultingPassword = "8";
+
+  // do a for loop
+  for (var i = 0; i < passwordlength; i++) {
+    var includedSpecial = false;
+    var includedNumeric = false;
+
+    if (includeSpecialCharacter && !includedSpecial) {
+      // add a special character randomly in the resultingPassword
+      includedSpecial = true;
+    }
+
+    
+  
+      
+    resultingPassword += '*';
+  }
+
 }
--- README copy.md
+++ README copy.md
@@ -1,1 +1,3 @@
 # Password Generator Starter Code
+This project is meant to provide you with a unique project, the page is supposed to provide you with a password that will inlcude a special character, upper case letters, loseercase letters and numeric symblow.
+
